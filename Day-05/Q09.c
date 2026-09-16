8992df168a491f14684ee2c8a727a40eb5aef49c
Bishwarup07 <bishwaruphalder6@gmail.com>
2026-09-16 14:39:00 +0530

    Day5

---
+++ Day-05/Q09.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float principal, rate, time;
+    float SI, CI;
+
+    // Input
+    scanf("%f %f %f", &principal, &rate, &time);
+
+    // Simple Interest
+    SI = (principal * rate * time) / 100;
+
+    // Compound Interest
+    CI = principal * pow((1 + rate / 100), time) - principal;
+
+    // Output
+    printf("Simple Interest=%.0f, Compound Interest=%.2f", SI, CI);
+
+    return 0;
+}
